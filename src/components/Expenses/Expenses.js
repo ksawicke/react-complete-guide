a237d7cb5bd0b8f5c979d051b0a7e0ec435a0700
Kevin Sawicke <kevinsawicke@gmail.com>
2022-03-01 20:42:37 -0700

    Add a form to handle adding new Expenses and work with state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,12 @@
-import ExpenseItem from "./ExpenseItem";
-import "./Expenses.css";
+import React from 'react';
 
-function Expenses(props) {
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './Expenses.css';
+
+const Expenses = (props) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -19,7 +22,7 @@ function Expenses(props) {
         amount={props.items[2].amount}
         date={props.items[2].date}
       />
-    </div>
+    </Card>
   );
 }
 
